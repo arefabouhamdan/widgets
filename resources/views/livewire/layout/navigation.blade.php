4f1f3658a8049f24356c352dd09745c703d9151f
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-29 19:53:57 +0200

    Blade <> Removed default headers in profile and dashboard blades

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -17,7 +17,7 @@ $logout = function (Logout $logout) {
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <div class="text-xl text-white">Widgets</div>
+                    <a href="dashboard" class="text-xl text-white">Widgets</a>
                 </div>
 
                 <!-- Navigation Links -->
@@ -30,7 +30,7 @@ $logout = function (Logout $logout) {
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-black bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
 
                             <div class="ms-1">
