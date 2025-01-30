d0c270f5663f28c88892e94b0295efed478603d3
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-30 14:21:33 +0200

    Livewire <> Added remaining widgets

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,10 +4,10 @@
     <div>
       <div class="bg-red-600 flex">
          <livewire:weather-widget />
-         <livewire:weather-widget />
-         <livewire:weather-widget />
-         <livewire:weather-widget />
-         <livewire:weather-widget />
+         <livewire:spotify-widget />
+         <livewire:forex-widget />
+         <livewire:news-widget />
+         <livewire:Music-widget />
       </div>
       <button class="bg-white p-6 rounded-full absolute z-6 bottom-5 right-5" style="background-color: #16526E;">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
---
+++ resources/views/livewire/music-widget.blade.php
@@ -0,0 +1,3 @@
+<div class="sidebar-widget p-4 text-white w-80 m-1 rounded" draggable="true" data-w="4" data-h="2" style="background-color: #16526E;">
+    Music Widget
+</div>
