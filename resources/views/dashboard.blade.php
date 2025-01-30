1c353939071736870342ffdba8115d3fd894d592
arefabouhamdan <arefabouhamdan@gmail.com>
2025-01-30 14:15:00 +0200

    Livewire & Tailwind <> Added plus button to open widgets topbar and changed widgets and topbar colors

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,18 @@
 <x-app-layout>
     <link href="https://cdn.jsdelivr.net/npm/gridstack@7.2.3/dist/gridstack.min.css" rel="stylesheet">
 
-    <div style="display: flex;">
-      <div class="bg-white" style="height:100vh;">
+    <div>
+      <div class="bg-red-600 flex">
+         <livewire:weather-widget />
+         <livewire:weather-widget />
+         <livewire:weather-widget />
+         <livewire:weather-widget />
          <livewire:weather-widget />
       </div>
-      <div class="w-full">
-        <div class="grid-stack w-full"></div>
-        <button class="p4 bg-white">+</button>
-      </div>
+      <button class="bg-white p-6 rounded-full absolute z-6 bottom-5 right-5" style="background-color: #16526E;">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-plus"><circle cx="12" cy="12" r="10"/><path d="M8 12h8"/><path d="M12 8v8"/></svg>
+      </button>
+      <div class="grid-stack "></div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/gridstack@7.2.3/dist/gridstack-all.js"></script>
